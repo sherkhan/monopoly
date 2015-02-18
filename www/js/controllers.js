08e42de163cc33d0d06ada08a51b1825f5d9f35c
Sherkhan <sherkhan@xakep.ru>
2015-02-19 00:07:22 +0300

    first

---
+++ www/js/controllers.js
@@ -0,0 +1,29 @@
+angular.module('starter.controllers', [])
+    .controller('addPlayer', function ($scope, $ionicModal) {
+
+        $ionicModal.fromTemplateUrl('templates/addplayer.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
+        $scope.openModal = function () {
+            $scope.modal.show();
+
+        };
+        $scope.closeModal = function () {
+            $scope.modal.hide();
+        };
+        //Cleanup the modal when we're done with it!
+        $scope.$on('$destroy', function () {
+            $scope.modal.remove();
+        });
+        // Execute action on hide modal
+        $scope.$on('modal.hidden', function () {
+            // Execute action
+        });
+        // Execute action on remove modal
+        $scope.$on('modal.removed', function () {
+            // Execute action
+        });
+    })
