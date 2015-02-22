32381e16caa18c7695b432c9be5ef755c7824c48
Sherkhan <sherkhan@xakep.ru>
2015-02-22 12:15:11 +0300

    players list worked

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,39 +1,38 @@
 angular.module('starter.controllers', [])
     .controller('addPlayer', function ($scope, $ionicModal) {
         $scope.players=[];
-        $scope.players.lists= [];
 
-        $ionicModal.fromTemplateUrl('templates/addplayer.html', {
+
+
+        $ionicModal.fromTemplateUrl('templates/addplayer.html', function(modal) {
+            $scope.addPlayer = modal;
+        }, {
             scope: $scope,
             animation: 'slide-in-up'
-        }).then(function (modal) {
-            $scope.modal = modal;
         });
+
+
         $scope.openModal = function () {
-            $scope.modal.show();
+            $scope.addPlayer.show();
 
         };
         $scope.closeModal = function () {
-            $scope.modal.hide();
+            $scope.addPlayer.hide();
         };
-        //Cleanup the modal when we're done with it!
-        $scope.$on('$destroy', function () {
-            $scope.modal.remove();
-        });
-        // Execute action on hide modal
-        $scope.$on('modal.hidden', function () {
-            // Execute action
-        });
-        // Execute action on remove modal
-        $scope.$on('modal.removed', function () {
-            // Execute action
-        });
 
-        $scope.playerAdd = function(players, list){
-            $scope.players.lists.push({name:$scope.list.name, money:$scope.list.name});
+
+        $scope.playerAdd = function(player){
+            $scope.players.push({
+                name: player.name,
+                money: player.money
+            });
+
             $scope.closeModal();
-            console.log($scope.players.lists)
+
+            console.log($scope.players)
         }
 
     })
+
+
 
